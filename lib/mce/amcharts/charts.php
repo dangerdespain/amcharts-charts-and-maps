8de988cd8c743d108398997de982a27cc07b5b62
martynasma <martynasma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-18 11:19:18 +0000

    1.0.3 Version
    git-svn-id: https://plugins.svn.wordpress.org/amcharts-charts-and-maps/trunk@950947 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/mce/amcharts/charts.php
+++ lib/mce/amcharts/charts.php
@@ -15,49 +15,139 @@ header('Content-Type: text/html; charset=' . get_bloginfo('charset'));
 wp_admin_css( 'wp-admin', true );
 ?>
 <style type="text/css">
-	body {
-		min-width: 0;
+  body {
+    min-width: 0;
     min-height: 0;
     height: auto;
     padding: 10px;
-	}
+  }
   
-  .chart {
+  .post {
     font-weight: bold;
     cursor: pointer;
     padding: 3px 6px;
   }
   
-  .chart:hover {
+  .post:hover {
     background-color: #eee;
+  }
+
+  .amcharts-tabs {
+    border-bottom: 2px solid #fff;
+    overflow: hidden;
+    margin-bottom: 10px;
+  }
+
+  .amcharts-tabs ul, .amcharts-tabs li {
+    margin: 0;
+  }
+
+  .amcharts-tabs a {
+    float: left;
+    padding: 5px 15px;
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .amcharts-tabs a.active {
+    background: #fff;
+  }
+
+  #live-editor-width, #live-editor-height {
+    width: 35%;
+  }
+
+  #live-editor-url.error {
+    border: 1px solid #d00;
+  }
+
+  input {
+    font-size: 13px;
   }
 
 </style>
 <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
 </head>
 <body>
-  <ul>
-  <?php
-  $charts = get_posts( array(
-    'post_type' => 'amchart'
-  ) );
-  foreach( $charts as $chart ) {
-		$id = $chart->ID;
-		if ( $slug = get_post_meta( $chart->ID, '_amcharts_slug', true ) )
-			$id = $slug;
-    ?>
-    <li class="chart" id="<?php echo $id; ?>"><?php echo $chart->post_title; ?></li>
-    <?php
-  }
-  ?>
-  </ul>
+  <div class="amcharts-tabs">
+    <ul>
+      <li><a href="#tabs-local" class="active"><?php _e( 'Local Charts', 'amcharts' ); ?></a></li>
+      <li><a href="#tabs-live"><?php _e( 'Live Editor', 'amcharts' ); ?></a></li>
+    </ul>
+  </div>
+  <div id="tabs-live" style="display: none;">
+    <p>
+      <input type="text" value="" id="live-editor-url" placeholder="<?php _e( 'Enter a URL to a chart created in Live Editor', 'amcharts' ); ?>" class="widefat" />
+    </p>
+    <p>
+      <input type="text" value="" id="live-editor-width" placeholder="<?php _e( 'Width (default: 400px)', 'amcharts' ); ?>" class="fat" />
+      <input type="text" value="" id="live-editor-height" placeholder="<?php _e( 'Height (default: 300px)', 'amcharts' ); ?>" class="fat" />
+      <input type="button" id="live-editor-ok" value="<?php _e( 'Insert', 'amcharts' ); ?>" class="button button-primary" />
+    </p>
+    <p class="description"><?php _e( 'Once you publish the chart in Live Editor, copy and paste it\'s URL into field above.', 'amcharts' ); ?></p>
+    <p class="description"><?php _e( 'I.e.: http://live.amcharts.com/NmU2Z/', 'amcharts' ); ?></p>
+    <p>
+      <input type="button" value="<?php _e( 'Open Live Editor', 'amcharts' ); ?>" onclick="window.open('http://live.amcharts.com/');" class="button" />
+      <span class="description"><?php _e( 'This will open Live Editor in a new window', 'amcharts' ); ?></span>
+    </p>
+
+  </div>
+  <div id="tabs-local">
+    <input type="text" value="" id="post-search" placeholder="<?php _e( 'Start typing to search', 'amcharts' ); ?>" class="widefat" />
+    <div id="results"></div>
+  </div>
   <script>
+  var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
   jQuery( function( $ ) {
-    $( '.chart' ).click( function () {
+    amchartsUpdateSearchResults();
+    $( '#results').on( 'click', '.post', function () {
       window.parent.tinyMCE.activeEditor.execCommand( 'mceInsertContent', false, '[amcharts id="' + this.id + '"]' );
       parent.tinyMCE.activeEditor.windowManager.close( window );
     } );
+    $( '#post-search' ).on('keyup change', function () {
+      amchartsUpdateSearchResults();
+    });
+    var currentTab = 'tabs-local';
+    $( '.amcharts-tabs a').on( 'click', function (e) {
+      var target = this.href.split('#')[1];
+      e.preventDefault();
+      if ( target == currentTab )
+        return;
+      $( '#' + currentTab ).hide();
+      $( '#' + target ).show();
+      $( '.amcharts-tabs a' ).removeClass( 'active' );
+      $( this ).addClass( 'active' );
+      currentTab = target;
+    });
+    $('#live-editor-ok').on('click', function () {
+      var url = $('#live-editor-url').val();
+      if ( '' == url ) {
+        $( '#live-editor-url' ).addClass( 'error' ).prop( 'placeholder', '<?php echo esc_js( __( 'Please enter a chart URL', 'amcharts' ) ); ?>' );
+        return;
+      }
+      var embed = '[embed';
+      var width = $( '#live-editor-width' ).val();
+      var height = $( '#live-editor-height' ).val();
+      if ( '' != width ) embed += ' width="' + width + '"';
+      if ( '' != height ) embed += ' height="' + height + '"';
+      embed += ']' + url + '[/embed]'
+      window.parent.tinyMCE.activeEditor.execCommand( 'mceInsertContent', false, embed );
+      parent.tinyMCE.activeEditor.windowManager.close( window );
+    });
+    $( '#live-editor-url' ).on('keyup change', function () {
+      $(this).removeClass( 'error' );
+    });
   } );
+  function amchartsUpdateSearchResults () {
+    var query = jQuery( '#post-search' ).val();
+    var data = {
+      'action': 'amcharts_get_posts',
+      'query': query
+    };
+    jQuery.post(ajaxurl, data, function(response) {
+      jQuery('#results').html(response);
+    });
+  }
   </script>
 </body>
 </html>
